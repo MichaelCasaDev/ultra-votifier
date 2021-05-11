5ee386ec34917f2a2c710dc967e7ecde65956656
MichaelDevC <57275048+MichaelDevC@users.noreply.github.com>
2021-05-11 20:17:03 +0200

    First commit
    Added all necessary files and also test!

---
+++ test/app.js
@@ -0,0 +1,22 @@
+const votifier = require('../src/votifier.js');
+
+
+var options = { // Parse all parameters to send the vote...
+    host: "127.0.0.1",
+    port: "3000",
+    token: "XXXXXXXXXXXXXXXXXX",
+    vote: {
+        username: "MichaelDevC",
+        address: '127.0.0.1',
+        timestamp: new Date().getTime(),
+        serviceName: "Your service name!"
+    }
+};
+
+votifier(options, (err) => { // Votifier function
+    if (err) { // Check if there's error and log it
+        console.log("🚨 Error from send vote function");
+    } else {
+        console.log("📡 Vote sent to the server  [ " + "MichaelDevC" + " ]");
+    };
+});
